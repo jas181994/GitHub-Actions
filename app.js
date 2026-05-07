14e46a6a3077fd14624f5237cda92e520441c790
jasmeet <jasmeet@Server.localdomain>
2026-05-07 14:44:14 +0530

    test

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('node appp deployed through Github actions tested on eks 070526 ')
+  res.send('node app deployed through Github actions tested on eks 070526 ')
 })
 
 app.listen(port, () => {
