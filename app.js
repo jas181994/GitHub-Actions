2ff949fb8b24d2c9df23c7272fcf4a10fa8e5542
jas181994 <jasmeet181994@gmail.com>
2026-05-07 09:59:34 +0530

    commit for eks

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('node app deployed through Github actions')
+  res.send('node app deployed through Github actions tested on eks 070526 ')
 })
 
 app.listen(port, () => {
